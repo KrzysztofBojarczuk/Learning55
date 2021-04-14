1ef0045f739ad306818b125901b0d66f0a910510
KrzysztofBojarczuk <69596796+KrzysztofBojarczuk@users.noreply.github.com>
2021-04-14 07:06:32 +0200

    Add project files.

---
+++ zliczanie liczb parzystych 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace zliczanie_liczb_parzystych_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int even = 0;
+            int odd = 0;
+            Random rand = new Random();
+            int userInput = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                userInput = rand.Next(0, 100);
+                if (userInput % 2 == 0) even++;
+                if (userInput % 2 == 1) odd++;
+                Console.WriteLine(userInput);
+
+
+            }
+            Console.WriteLine($"Parzyste {even} Nieparzyste {odd}");
+        }
+    }
+}
